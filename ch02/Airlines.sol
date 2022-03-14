274e691c03daed6bfdc7976e2d2674c21e48c0f4
EunsangJeon <eunsang.jeon.127@gmail.com>
2022-03-14 10:44:26 +0900

    updated methods

--- ch02/Airlines.sol
+++ ch02/Airlines.sol
@@ -28,4 +28,50 @@ contract Airlines {
         membership[msg.sender] = 1;
         balanceDetails[msg.sender].escrow = msg.value;
     }
+
+    function register() public payable {
+        address AirlineA = msg.sender;
+        membership[AirlineA] = 1;
+        balanceDetails[msg.sender].escrow = msg.value;
+    }
+
+    function unregister(address payable AirlineZ) onlyChairPerson public {
+        if (chairperson != msg.sender) {
+            revert();
+        }
+
+        membership[AirlineZ] = 0;
+        AirlineZ.transfer(balanceDetails[AirlineZ].escrow);
+        balanceDetails[AirlineZ].escrow = 0;
+    }
+
+    function request(address toAirline, uint hashOfDetails) onlyMember public {
+        if (membership[toAirline] != 1) {
+            revert();
+        }
+
+        balanceDetails[msg.sender].status = 0;
+        balanceDetails[msg.sender].hashOfDetails = hashOfDetails;
+    }
+
+    function response(address fromAirline, uint hashOfDetails, uint done) onlyMember public {
+        if (membership[fromAirline] != 1) {
+            revert();
+        }
+
+        balanceDetails[msg.sender].status = done;
+        balanceDetails[fromAirline].hashOfDetails = hashOfDetails;
+    }
+
+    function settlePayment(address payable toAirline) onlyMember payable public {
+
+        address fromAirline = msg.sender;
+        uint amt = msg.value;
+        if (balanceDetails[fromAirline].escrow < amt) {
+            revert();
+        }
+        balanceDetails[toAirline].escrow += amt;
+        balanceDetails[fromAirline].escrow -= amt;
+        toAirline.transfer(amt);
+    }
 }
