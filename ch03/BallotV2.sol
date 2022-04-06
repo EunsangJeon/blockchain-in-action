c3be19a784390a04172a554857574b8562ce702b
EunsangJeon <eunsang.jeon.127@gmail.com>
2022-04-06 13:57:54 +0900

    added constructor and changeState()

--- ch03/BallotV2.sol
+++ ch03/BallotV2.sol
@@ -22,4 +22,23 @@ contract BallotV2 {
     }
 
     Phase public state = Phase.Init;
+
+    constructor(uint numProposals) public {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 2;
+
+        for (uint prop = 0; prop < numProposals; prop++) {
+            proposals.push(Proposal(0));
+        }
+    }
+
+    function changeState(Phase x) public {
+        if (msg.sender != chairperson) {
+            revert();
+        }
+        if (x < state) {
+            revert();
+        }
+        state = x;
+    }
 }
