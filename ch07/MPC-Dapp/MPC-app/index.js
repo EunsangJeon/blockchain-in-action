8c639b8255bdc4dc540cd02cf007d3b7b2c385f7
EunsangJeon <eunsang.jeon.127@gmail.com>
2022-04-13 10:12:05 +0900

    basic refactoring for ch07 init code

--- ch07/MPC-Dapp/MPC-app/index.js
+++ ch07/MPC-Dapp/MPC-app/index.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 app.use(express.static('src'));
 app.use(express.static('../MPC-contract/build/contracts'));
 app.get('/', function (req, res) {
-  res.render('index.html');
+    res.render('index.html');
 });
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
